03b3f5df0d5a555cc3204f0158984b3bb75b39ac
Daniel Peck <peck@danielpeck.com>
2020-05-29 13:13:31 -0400

    in prog

---
+++ src/test/scala/helloworld/HelloWorldSimulation.scala
@@ -0,0 +1,88 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.util.Random
+
+
+class HelloWorldSimulation extends Simulation {
+
+  val feeder = Iterator.continually(
+    Map("email" -> (Random.alphanumeric.take(20).mkString + "@foo.com"),
+      "user_name" -> (Random.alphanumeric.take(20).mkString)
+    )
+  )
+
+
+  val httpProtocol = http
+    .baseUrl("https://dev.sugarcaneatl.com")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.9")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36")
+    .wsBaseUrl("wss://dev.sugarcaneatl.com")
+
+  val scn = scenario("BasicSimulation")
+    .feed(feeder)
+    .exec(http("Root")
+            .get("/")
+            .check(css("meta[name=csrf-token]", "content").saveAs("csrf_token"))
+    )
+    .pause(1 second, 10 seconds)
+    .exec(http("Age Verification")
+            .post("/age_verification")
+            .formParam("_csrf_token", "${csrf_token}")
+            .formParam("age_verification[birth_date][month]", "5")
+            .formParam("age_verification[birth_date][day]", "28")
+            .formParam("age_verification[birth_date][year]", "1999")
+    )
+    .pause(1 second, 300 seconds)
+    .exec(http("User Registration Edit")
+            .get("/users/register")
+            .check(css("meta[name=csrf-token]", "content").saveAs("csrf_token"))
+    )
+    .pause(1 second, 10 seconds)
+    .exec(http("User Registration Create")
+          .post("/users/register")
+          .formParam("_csrf_token", "${csrf_token}")
+          .formParam("user[user_name]", "${user_name}")
+            .formParam("user[first_name]", "Test")
+            .formParam("user[last_name]", "Test")
+            .formParam("user[email]", "${email}")
+            .formParam("user[password]", "password")
+    )
+    .pause(1 second, 10 seconds)
+    .exec(http("Live Stream")
+            .get("/zCzLPJwyD6rm42mhsZVcEZCXmN00vB9xR/live")
+            .check(css("meta[name=csrf-token]", "content").saveAs("csrf_token"))
+            .check(css("div[data-phx-main=true]", "data-phx-session").saveAs("phx_session"))
+            .check(css("div[data-phx-main=true]", "data-phx-static").saveAs("phx_static"))
+            .check(css("div[data-phx-main=true]", "id").saveAs("phx_id"))
+    )
+    .pause(1 second, 5 seconds)
+    .exec(ws("Connect WS").connect("/live/websocket?_csrf_token=${csrf_token}&vsn=2.0.0"))
+    .pause(1 second, 5 seconds)
+    .exec(ws("Join LiveView Channel")
+            .sendText("""
+["4", "4", "lv:${phx_id}", "phx_join", {"url": "http://localhost:4000/cA2gVqg02MArWAiQvyfwsT1cP4mWBM00hb/live", "static": "${phx_static}", "session": "${phx_session}", "params": {"_csrf_token": "${csrf_token}"}}]
+""")
+    )
+    .pause(1 second, 10 seconds)
+    .exec(ws("LiveView Send Join Event")
+            .sendText("""
+["4", "5", "lv:${phx_id}", "event", {"type": "form", "event": "new_message", "value": "_csrf_token=${csrf_token}&message[body]=test%20message"}]
+""")
+    )
+    .repeat(10, "count") {
+      exec(ws("Websocket Heartbeat")
+             .sendText("""[null, ${count}, "phoenix", "heartbeat", {}]""")
+      )
+        .pause(30)
+    }
+
+  setUp(
+    scn.inject(atOnceUsers(2000))
+  ).protocols(httpProtocol)
+
+}
